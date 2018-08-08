7d7e151edbe611ed2aa9fa49d29e85e9dcaf4c24
roldma <roldma@farmingdale.edu>
2018-08-08 12:36:26 -0400

    prettified

--- schema/albumModel.js
+++ schema/albumModel.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+
+const { Schema } = mongoose;
 const artist = require('./artistModel');
 
 console.log(artist.artistModel);
 
-let albumSchema = new Schema({
-	name: {type: String, required: true}, 
-	artist: [artist.artistModel],
-	songs: {type: Array, required: true},
-})
+const albumSchema = new Schema({
+  name: { type: String, required: true },
+  artist: [artist.artistModel],
+  songs: { type: Array, required: true },
+});
 
-module.exports = { model: mongoose.model('album', albumSchema),
-				   albumSchema: albumSchema
- }
+module.exports = {
+  model: mongoose.model('album', albumSchema),
+  albumSchema,
+};
--- schema/dummyData.js
+++ schema/dummyData.js
@@ -1,42 +1,42 @@
-let artist = [
-	{
-		name:'Muse',
-		albums:[albums[0],albums[1]]
-	},
-	{
-		name:'Westlife',
-		albums:[albums[0],albums[1]]
-	}
+const artist = [
+  {
+    name: 'Muse',
+    albums: [albums[0], albums[1]],
+  },
+  {
+    name: 'Westlife',
+    albums: [albums[0], albums[1]],
+  },
 ];
 
 let albums = [
-	{
-		name: 'Absolution', 
-		artist: artist[0],
-		songs: songs[1],
-	},	
-	{
-		name: 'CoastToCoast', 
-		artist: artist[1],
-		songs: songs[0],
-	}	
-]
+  {
+    name: 'Absolution',
+    artist: artist[0],
+    songs: songs[1],
+  },
+  {
+    name: 'CoastToCoast',
+    artist: artist[1],
+    songs: songs[0],
+  },
+];
 
 let songs = [
-	{
-		name: 'UptownGirl', 
-		artist: artist[1],
-		album: albums[1],
-	},
-	{
-		name: 'Stockholm Syndrome', 
-		artist: artist[0],
-		album: albums[0],
-	}
-]
+  {
+    name: 'UptownGirl',
+    artist: artist[1],
+    album: albums[1],
+  },
+  {
+    name: 'Stockholm Syndrome',
+    artist: artist[0],
+    album: albums[0],
+  },
+];
 
 module.exports = {
-	artists,
-	albums,
-	songs
-}
+  artists,
+  albums,
+  songs,
+};
